a6571942b21e2009acac6a692d887697d2a1da05
Ali Ariff <ali.ariff12@gmail.com>
2017-06-13 16:00:47 +0200

    init spring

---
+++ ex2/CommentAPI/src/test/java/de/rwth/webtech/npe/CommentAPI/CommentApiApplicationTests.java
@@ -0,0 +1,16 @@
+package de.rwth.webtech.npe.CommentAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommentApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
